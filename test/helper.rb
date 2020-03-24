04e87e04375ee2746f1e5e719dd5b365a3a186cf
Ilana Corson <ilana.corson@gmail.com>
2020-03-24 11:20:04 -0700

    patch for Rails 4.2 and Ruby 2.6 Thread error only for this combo
    Reference: https://github.com/rails/rails/issues/34790

--- test/helper.rb
+++ test/helper.rb
@@ -50,6 +50,21 @@ class ActiveSupport::TestCase
 end
 
 class ActionController::TestCase
+  if RUBY_VERSION>='2.6.0'
+    if Rails.version < '5'
+      class ActionController::TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          # hack to avoid MonitorMixin double-initialize error:
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
+      end
+    else
+      puts "Monkeypatch for ActionController::TestResponse no longer needed"
+    end
+  end
+  
   def setup
     @routes = TestApp.routes
   end
