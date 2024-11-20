81d1a0288016c7d8e770bb11b5a67189fd73b733
Benjamin Quorning <benjamin@quorning.net>
2024-11-20 18:37:41 +0100

    Remove actionpack-action_caching from test setup
    The `caches_action` call was added in one of the first commits, when we
    were testing against Rails 2.3 -- long before actionpack-action_caching
    was removed from Rails and published as a separate gem. A gem which has
    not been updated for three years and which is compatible with Rails 6.1
    and older.

--- test/jsonp_test.rb
+++ test/jsonp_test.rb
@@ -3,7 +3,6 @@ require File.expand_path("helper", File.dirname(__FILE__))
 class JSONPControllerTester < ActionController::Base
   include Charcoal::JSONP
 
-  caches_action :test
   allow_jsonp :test
 
   def test
